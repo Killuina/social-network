a989150d3d52cd6b2d488203058780ee5d99a56d
Cristina Jiménez <cristinajm98@hotmail.com>
2024-03-02 12:41:37 +0100

    Add min and max with to UserCard

--- src/components/UserCard/UserCardStyled.ts
+++ src/components/UserCard/UserCardStyled.ts
@@ -4,4 +4,6 @@ export const UserCardStyled = styled.article`
   padding: 10px;
   background-color: aliceblue;
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  min-width: 300px;
+  max-width: 500px;
 `;
